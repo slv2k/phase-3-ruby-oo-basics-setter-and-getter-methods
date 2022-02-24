83f754e6fd3bf6547b0bde9464dde5e24850d789
samuel <samuelmmanor@gmail.com>
2022-02-24 15:03:04 -0500

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
+    # name setter method
+    def name=(name)
+        @name = name
+    end
 
+    # name getter method
+    def name
+        @name
+    end
+
+    # job setter method
+    def job=(job)
+        @job = job
+    end
+
+    # job getter method
+    def job
+        @job
+    end
 end
