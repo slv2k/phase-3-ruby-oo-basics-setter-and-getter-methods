bc356bf362d85df70ef102e51bb0b5970e993d18
samuel <samuelmmanor@gmail.com>
2022-02-24 15:02:55 -0500

    commit tree ecd2845c1c64ae88bb54c9a43f6f70feedf8683d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    # name setter method
+    def name=(name)
+        @name = name
+    end
 
+    # name getter method
+    def name
+        @name
+    end
+
+    # breed setter method
+    def breed=(breed)
+        @breed = breed
+    end
+
+    # breed getter method
+    def breed
+        @breed
+    end
 end
